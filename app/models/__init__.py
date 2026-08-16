8f6a4bc0a517cdb8aa5fb3456cec304b4d7a924f
CommTax <pillayadesh04@gmail.com>
2026-08-17 00:31:46 +0530

    Import models and define __all__ for exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,16 @@
+from app.models.user import UserCreate, UserResponse
+from app.models.session import SessionCreate, SessionResponse
+from app.models.question import QuestionResponse
+from app.models.response import ResponseSubmit, VoiceAnalysisRequest, AssessmentCompleteRequest, AssessmentResult
 
+__all__ = [
+    'UserCreate',
+    'UserResponse',
+    'SessionCreate',
+    'SessionResponse',
+    'QuestionResponse',
+    'ResponseSubmit',
+    'VoiceAnalysisRequest',
+    'AssessmentCompleteRequest',
+    'AssessmentResult'
+]

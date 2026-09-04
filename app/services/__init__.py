053f8197bb5ea888b04ffdcaedfd478d2f2d24a7
CommTax <pillayadesh04@gmail.com>
2026-09-04 09:32:59 +0530

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,2 @@
 # This file makes the services directory a Python package
-from . import persona_service, lead_service, analysis_service, gemini_client
-from app.services.archetype_engine import ArchetypeEngine
-
-__all__ = ['ArchetypeEngine']
+from . import persona_service, lead_service, analysis_service
